0b01f4c21dd7247bdf6b63e6ea950ced3bd90997
Héctor Sab <hector.sab@hotmail.com>
2018-08-24 15:22:32 -0500

    .

--- src/runme_predict_and_calculate_perf_variable_th.py
+++ src/runme_predict_and_calculate_perf_variable_th.py
@@ -6,42 +6,46 @@ from model import Model
 
 import utils as ut
 
-im_dir = '/home/hectorsab/data/Documents/CICATA/yolo/CICATA_dataset/416_416/00000001/'
-out_dir = '/home/hectorsab/data/Documents/CICATA/yolo/CICATA_dataset/'
-out_dir += '416_416/{}/00000001_predicted_pTH-{}_sTH-{}/'
+main_dir = '/data/HectorSanchez/respaldo/Documents/CICATA/Test/yolo/CICATA_dataset/416_416/'
+im_dir = main_dir+'00000001/'
+out_dir = main_dir+'{}/00000001_predicted_pTH-{:.2f}_sTH-{:.2f}/'
+
+# Test name
+t_name = 'test_01'
 
 if __name__=='__main__':
-	# Test name
-	t_name = 'test_01'
-	model = Model()
-
-	# Loads the images paths
-	imfpaths = sorted(os.listdir(im_dir))
-	imfpaths = ut.check_valid_files(imfpaths,'jpg')
-
 	# Controls the prob th in the prediction model
 	pTH = np.linspace(0,1,21)
 
 	# Controls minimum iou th for the nms
 	sTH = np.linspace(0,1,21)
 
-	pbar = tqdm(range(pTH.shape[0]*sTH.shape[0]*len(imfpaths)))
-	# Predict all the bboxes
-	# Iterates over all the probs THs
-	for p in pTH:
-		# Iterates over all the ious THs
-		for s in sTH:
-			final_out_dir = out_dir.format(t_name,p,s)
-			if not os.path.isdir(final_out_dir):
-				os.makedirs(final_out_dir)
-			for i in range(len(imfpaths)):
-				imfpath = imfpaths[i]
-				im = ut.load_im(im_dir+imfpath,new_shape=(416,416))
-				coord,lbs,ob_mask,pobj = model.predict(im,TH_prob=p)
-				filtered_objects = ut.nms_yolo(coord,lbs,ob_mask,pobj,TH=s)
-				ut.prediction2kitti2(filtered_objects,final_out_dir,[imfpath[:-3]+'txt'])
-				pbar.update(1)
-	pbar.close()
+	if False:
+		model = Model()
+
+		# Loads the images paths
+		imfpaths = sorted(os.listdir(im_dir))
+		imfpaths = ut.check_valid_files(imfpaths,'jpg')
+
+
+		pbar = tqdm(range(pTH.shape[0]*sTH.shape[0]*len(imfpaths)))
+		# Predict all the bboxes
+		# Iterates over all the probs THs
+		for p in pTH:
+			# Iterates over all the ious THs
+			for s in sTH:
+				final_out_dir = out_dir.format(t_name,p,s)
+				if not os.path.isdir(final_out_dir):
+					os.makedirs(final_out_dir)
+				for i in range(len(imfpaths)):
+					imfpath = imfpaths[i]
+					im = ut.load_im(im_dir+imfpath,new_shape=(416,416))
+					coord,lbs,ob_mask,pobj = model.predict(im,TH_prob=p)
+					filtered_objects = ut.nms_yolo(coord,lbs,ob_mask,pobj,TH=s)
+					ut.prediction2kitti2(filtered_objects,final_out_dir,[imfpath[:-3]+'txt'])
+					pbar.update(1)
+		pbar.close()
+
 
 	# Calculate performance
 	gt_dir = im_dir
@@ -62,5 +66,5 @@ if __name__=='__main__':
 			pbar.update(1)
 	pbar.close()
 
-	with open('full_results.pk','w') as f:
+	with open(os.path.join(main_dir,t_name,'full_results.pk'),'wb') as f:
 		pickle.dump(results,f)
